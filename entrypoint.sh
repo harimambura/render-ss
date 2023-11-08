48c4d79c3497553685b74d440e3e9e4a9e3ebc69
harimambura <harimambura@gmail.com>
2023-11-08 17:58:00 +0300

    tt4

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-#v2ray-plugin版本
 if [[ -z "${VER}" ]]; then
   VER="latest"
 fi
@@ -19,10 +18,10 @@ if [[ -z "${ProxySite}" ]]; then
   ProxySite="www.ietf.org"
 fi
 
-if [[ -z "${V2_Path}" ]]; then
-  V2_Path="/s233"
+if [[ -z "${X_Path}" ]]; then
+  X_Path="/s233"
 fi
-echo ${V2_Path}
+echo ${X_Path}
 
 if [[ -z "${QR_Path}" ]]; then
   QR_Path="/qr_img"
@@ -31,21 +30,21 @@ echo ${QR_Path}
 
 
 if [ "$VER" = "latest" ]; then
-  V_VER=`wget -qO- "https://api.github.com/repos/shadowsocks/v2ray-plugin/releases/latest" | sed -n -r -e 's/.*"tag_name".+?"([vV0-9\.]+?)".*/\1/p'`
-  [[ -z "${V_VER}" ]] && V_VER="v1.3.0"
+  V_VER=`wget -qO- "https://api.github.com/repos/teddysun/xray-plugin/releases/latest" | sed -n -r -e 's/.*"tag_name".+?"([vV0-9\.]+?)".*/\1/p'`
+  [[ -z "${V_VER}" ]] && V_VER="v1.8.4"
 else
   V_VER="v$VER"
 fi
 
-mkdir /v2raybin
-cd /v2raybin
-V2RAY_URL="https://github.com/shadowsocks/v2ray-plugin/releases/download/${V_VER}/v2ray-plugin-linux-amd64-${V_VER}.tar.gz"
-echo ${V2RAY_URL}
-wget --no-check-certificate ${V2RAY_URL}
-tar -zxvf v2ray-plugin-linux-amd64-$V_VER.tar.gz
-rm -rf v2ray-plugin-linux-amd64-$V_VER.tar.gz
-mv v2ray-plugin_linux_amd64 /v2ray-plugin
-rm -rf /v2raybin
+mkdir /xraybin
+cd /xraybin
+XRAY_URL="https://github.com/teddysun/xray-plugin/releases/download/${V_VER}/xray-plugin-linux-amd64-${V_VER}.tar.gz"
+echo ${XRAY_URL}
+wget --no-check-certificate ${XRAY_URL}
+tar -zxvf xray-plugin-linux-amd64-$V_VER.tar.gz
+rm -rf xray-plugin-linux-amd64-$V_VER.tar.gz
+mv xray-plugin_linux_amd64 /xray-plugin
+rm -rf /xraybin
 
 cd /wwwroot
 tar xvf wwwroot.tar.gz
@@ -59,7 +58,7 @@ fi
 sed -e "/^#/d"\
     -e "s/\${PASSWORD}/${PASSWORD}/g"\
     -e "s/\${ENCRYPT}/${ENCRYPT}/g"\
-    -e "s|\${V2_Path}|${V2_Path}|g"\
+    -e "s|\${X_Path}|${X_Path}|g"\
     /conf/shadowsocks-libev_config.json >  /etc/shadowsocks-libev/config.json
 echo /etc/shadowsocks-libev/config.json
 cat /etc/shadowsocks-libev/config.json
@@ -74,7 +73,7 @@ fi
 
 sed -e "/^#/d"\
     -e "s/\${PORT}/${PORT}/g"\
-    -e "s|\${V2_Path}|${V2_Path}|g"\
+    -e "s|\${X_Path}|${X_Path}|g"\
     -e "s|\${QR_Path}|${QR_Path}|g"\
     -e "$s"\
     /conf/nginx_ss.conf > /etc/nginx/conf.d/ss.conf
@@ -86,13 +85,13 @@ if [ "$AppName" = "no" ]; then
   echo "不生成二维码"
 else
   [ ! -d /wwwroot/${QR_Path} ] && mkdir /wwwroot/${QR_Path}
-  plugin=$(echo -n "v2ray;path=${V2_Path};host=${AppName}.onrender.com;tls" | sed -e 's/\//%2F/g' -e 's/=/%3D/g' -e 's/;/%3B/g')
+  plugin=$(echo -n "xray;path=${X_Path};host=${AppName}.onrender.com;tls" | sed -e 's/\//%2F/g' -e 's/=/%3D/g' -e 's/;/%3B/g')
   ss="ss://$(echo -n ${ENCRYPT}:${PASSWORD} | base64 -w 0)@${AppName}.onrender.com:443?plugin=${plugin}" 
   echo "${ss}" | tr -d '\n' > /wwwroot/${QR_Path}/index.html
   echo -n "${ss}" | qrencode -s 6 -o /wwwroot/${QR_Path}/vpn.png
 fi
 
 echo 'RUN SS SERVER'
-/root/go/bin/go-shadowsocks2 -s "ss://AEAD_CHACHA20_POLY1305:${PASSWORD}@:443" -verbose -plugin v2ray-plugin -plugin-opts "server" -udp=false
+/root/go/bin/go-shadowsocks2 -s "ss://AEAD_CHACHA20_POLY1305:${PASSWORD}@:443" -verbose -plugin xray-plugin -plugin-opts "server" -udp=false
 rm -rf /etc/nginx/sites-enabled/default
 nginx -g 'daemon off;'
