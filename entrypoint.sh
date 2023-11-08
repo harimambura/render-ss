e5e3733bb1e188a6eb685bf2e6a5a0689a478d28
harimambura <harimambura@gmail.com>
2023-11-08 20:32:47 +0300

    prev

--- entrypoint.sh
+++ entrypoint.sh
@@ -50,9 +50,49 @@ cd /wwwroot
 tar xvf wwwroot.tar.gz
 rm -rf wwwroot.tar.gz
 
-plugin=$(echo -n "v2ray;path=${X_Path};host=${AppName}.onrender.com;tls" | sed -e 's/\//%2F/g' -e 's/=/%3D/g' -e 's/;/%3B/g')
-ss="ss://$(echo -n ${ENCRYPT}:${PASSWORD} | base64 -w 0)@${AppName}.onrender.com:443?plugin=${plugin}" 
-echo ${ss}
+if [ ! -d /etc/shadowsocks-libev ]; then  
+  mkdir /etc/shadowsocks-libev
+fi
+
+# TODO: bug when PASSWORD contain '/'
+sed -e "/^#/d"\
+    -e "s/\${PASSWORD}/${PASSWORD}/g"\
+    -e "s/\${ENCRYPT}/${ENCRYPT}/g"\
+    -e "s|\${X_Path}|${X_Path}|g"\
+    /conf/shadowsocks-libev_config.json >  /etc/shadowsocks-libev/config.json
+echo /etc/shadowsocks-libev/config.json
+cat /etc/shadowsocks-libev/config.json
+
+if [[ -z "${ProxySite}" ]]; then
+  s="s/proxy_pass/#proxy_pass/g"
+  echo "site:use local wwwroot html"
+else
+  s="s|\${ProxySite}|${ProxySite}|g"
+  echo "site: ${ProxySite}"
+fi
+
+sed -e "/^#/d"\
+    -e "s/\${PORT}/${PORT}/g"\
+    -e "s|\${X_Path}|${X_Path}|g"\
+    -e "s|\${QR_Path}|${QR_Path}|g"\
+    -e "$s"\
+    /conf/nginx_ss.conf > /etc/nginx/conf.d/ss.conf
+echo /etc/nginx/conf.d/ss.conf
+cat /etc/nginx/conf.d/ss.conf
+
+
+if [ "$AppName" = "no" ]; then
+  echo "不生成二维码"
+else
+  [ ! -d /wwwroot/${QR_Path} ] && mkdir /wwwroot/${QR_Path}
+  plugin=$(echo -n "xray;path=${X_Path};host=${AppName}.onrender.com;tls" | sed -e 's/\//%2F/g' -e 's/=/%3D/g' -e 's/;/%3B/g')
+  ss="ss://$(echo -n ${ENCRYPT}:${PASSWORD} | base64 -w 0)@${AppName}.onrender.com:443?plugin=${plugin}" 
+  echo "${ss}" | tr -d '\n' > /wwwroot/${QR_Path}/index.html
+  echo -n "${ss}" | qrencode -s 6 -o /wwwroot/${QR_Path}/vpn.png
+  echo ${ss}
+fi
 
 echo 'RUN SS SERVER'
-/root/go/bin/go-shadowsocks2 -s "ss://AEAD_CHACHA20_POLY1305:${PASSWORD}@:443" -plugin xx-plugin -plugin-opts "server;loglevel=none" -udp=false &
+/root/go/bin/go-shadowsocks2 -s "ss://AEAD_CHACHA20_POLY1305:${PASSWORD}@:443" -plugin xx-plugin -plugin-opts "server;loglevel=none" -udp=false >> /dev/null
+rm -rf /etc/nginx/sites-enabled/default
+nginx -g 'daemon off;'
